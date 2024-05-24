63dcb6f64bdea199653f63e84c1562d6a173a733
MariaB <peggy-sju@ya.ru>
2024-05-24 18:51:07 +0300

    Добавить функцию loadFromFile и добавить тестирование ее в main

--- src/ru/yandex/javacource/bochkareva/schedule/manager/FileBackedTaskManager.java
+++ src/ru/yandex/javacource/bochkareva/schedule/manager/FileBackedTaskManager.java
@@ -4,13 +4,17 @@ import ru.yandex.javacource.bochkareva.schedule.exceptions.ManagerSaveException;
 import ru.yandex.javacource.bochkareva.schedule.task.Epic;
 import ru.yandex.javacource.bochkareva.schedule.task.Subtask;
 import ru.yandex.javacource.bochkareva.schedule.task.Task;
+import ru.yandex.javacource.bochkareva.schedule.task.TaskStatus;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
 
@@ -19,6 +23,54 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     public FileBackedTaskManager(String filename) {
         super();
         this.filename = filename;
+    }
+
+    static FileBackedTaskManager loadFromFile(File file) {
+        FileBackedTaskManager taskManager = new FileBackedTaskManager(file.getAbsolutePath());
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+            String firstLine = fileReader.readLine();
+            ArrayList<Task> tasks = new ArrayList<>();
+            ArrayList<Epic> epics = new ArrayList<>();
+            ArrayList<Subtask> subtasks = new ArrayList<>();
+            while (fileReader.ready()) {
+                String newLine = fileReader.readLine();
+                String[] taskInfo = newLine.split(",", -1);
+                System.out.println(Arrays.toString(taskInfo));
+                Task newTask = new Task(Integer.parseInt(taskInfo[0]), taskInfo[2], taskInfo[4], TaskStatus.valueOf(taskInfo[3]));
+                switch (taskInfo[1]) {
+                    case "TASK":
+                        tasks.add(newTask);
+                        break;
+                    case "EPIC":
+                        Epic newEpic = new Epic(newTask);
+                        epics.add(newEpic);
+                        break;
+                    case "SUBTASK":
+                        Subtask newSubtask = new Subtask(newTask, Integer.parseInt(taskInfo[5]));
+                        subtasks.add(newSubtask);
+                        break;
+                }
+            }
+            for (Task task : tasks) {
+                taskManager.addTask(task);
+            }
+            HashMap<Integer, Integer> mapEpicKeys = new HashMap<>();
+            for (Epic epic : epics) {
+                int newEpicId = taskManager.addEpic(epic);
+                mapEpicKeys.put(epic.getId(), newEpicId);
+            }
+            for (Subtask subtask : subtasks) {
+                subtask.setEpicId(mapEpicKeys.get(subtask.getEpicId()));
+                taskManager.addSubtask(subtask);
+            }
+        } catch (FileNotFoundException e) {
+            throw new ManagerSaveException("Файл для чтения не найден.");
+        } catch (IOException e) {
+            throw new ManagerSaveException("Произошла ошибка во время чтения файла.");
+        }
+
+        return taskManager;
     }
 
     private void save() {
@@ -143,5 +195,32 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
         Task taskToEpic2Sprint7 = new Task(1, "Задача-эпик 2");
         Epic epic2Sprint7 = new Epic(taskToEpic2Sprint7);
         int epic2IdSprint7 = taskManager.addEpic(epic2Sprint7);
+
+        FileBackedTaskManager taskManager2 = FileBackedTaskManager.loadFromFile(new File("C:\\Users\\PC#05\\Desktop\\Test Git\\mytest.txt"));
+        printAllTasks(taskManager2);
+    }
+
+    private static void printAllTasks(TaskManager manager) {
+        System.out.println("Задачи:");
+        for (Task task : manager.getTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("Эпики:");
+        for (Task epic : manager.getEpics()) {
+            System.out.println(epic);
+
+            for (Task task : manager.getSubtasksOfEpic(epic.getId())) {
+                System.out.println("--> " + task);
+            }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : manager.getSubtasks()) {
+            System.out.println(subtask);
+        }
+
+        System.out.println("История:");
+        for (Task task : manager.getHistory()) {
+            System.out.println(task);
+        }
     }
 }
